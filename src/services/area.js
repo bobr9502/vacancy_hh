1111edfdf9d854a01cb67a9f794ab7876ce6af5a
Andrey Vinokurov <vinokurov_and@mail.ru>
2019-04-04 01:44:02 +0300

    Refactoring

--- src/services/area.js
+++ src/services/area.js
@@ -1,25 +1,21 @@
 import Api from "./services";
 
 class Area {
-    getApi() {
-        return Api.getArea();
-    }
+  getApi() {
+    return Api.getArea();
+  }
 
-    getArea(vacancy) {
-        const areaCountryName = vacancy
-            .map(function (item) {
-                return item.area.main_parent;
-            })
-            .filter(onlyUniqueForProp);
-        return areaCountryName;
-    }
+  getArea(vacancy) {
+    const areaCountryName = vacancy
+      .map(({ area }) => area.main_parent)
+      .filter(onlyUniqueForProp);
+    return areaCountryName;
+  }
 }
 
 function onlyUniqueForProp(value, index, self) {
-    const temp = self.map(function (item) {
-        return item.id;
-    });
-    return temp.indexOf(value.id) === index;
+  const temp = self.map(({ id }) => id);
+  return temp.indexOf(value.id) === index;
 }
 
 export default new Area();
