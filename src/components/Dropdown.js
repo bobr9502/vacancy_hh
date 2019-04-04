8d193a1d27182cf6b64679dcb73885867eaf0527
Andrey Vinokurov <vinokurov_and@mail.ru>
2019-04-05 00:00:51 +0300

    defaultPropsDropDown handed down to const. Add defaultProps and propTypes

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,39 +1,46 @@
 import _ from "lodash";
-import React, {Component} from "react";
-import {Dropdown} from "semantic-ui-react";
+import React, { Component } from "react";
+import { Dropdown } from "semantic-ui-react";
 import PropTypes from "prop-types";
 
-class DropdownExampleSearchSelectionTwo extends Component {
-    render() {
-        const {itemsArea, value, onChange} = this.props;
+const defaultPropsDropDown = [{ key: -1, text: "Все", value: "" }];
 
-        const stateOptions = _.map(itemsArea, item => ({
-            key: item.id,
-            text: item.name,
-            value: item.id
-        })).sort();
-        stateOptions.unshift({key: -1, text: "Все", value: ""});
-        return (
-            <Dropdown
-                value={value}
-                placeholder="Страна"
-                onChange={onChange}
-                search
-                selection
-                options={stateOptions}
-            />
-        );
-    }
+class DropdownExampleSearchSelectionTwo extends Component {
+  render() {
+    const { itemsArea, value, onChange } = this.props;
+    let stateOptions = _.map(itemsArea, item => ({
+      key: item.id,
+      text: item.name,
+      value: item.id
+    }));
+    stateOptions = [...stateOptions, ...defaultPropsDropDown].sort(
+      item => item.key
+    );
+    return (
+      <Dropdown
+        value={value}
+        placeholder="Страна"
+        onChange={onChange}
+        search
+        selection
+        options={stateOptions}
+      />
+    );
+  }
 }
 
 DropdownExampleSearchSelectionTwo.propTypes = {
-    itemsArea: PropTypes.array.isRequired,
-    onChange: PropTypes.func.isRequired,
-    value: PropTypes.string
+  itemsArea: PropTypes.array.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string
 };
 
-DropdownExampleSearchSelectionTwo.propTypes = {
-
-}
+DropdownExampleSearchSelectionTwo.defaultProps = {
+  itemsArea: defaultPropsDropDown,
+  value: "-1",
+  onChange: () => {
+    console.log("Error: Not found function onChange");
+  }
+};
 
 export default DropdownExampleSearchSelectionTwo;
