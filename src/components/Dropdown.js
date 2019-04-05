7a55dfa12007767013925b38e664693b97b85f1c
Andrey Vinokurov <vinokurov_and@mail.ru>
2019-04-05 23:20:28 +0300

    Класс в функцию т.к. компонент не имеет состояния (state) и ссылок(refs)

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,32 +1,30 @@
 import _ from "lodash";
-import React, { Component } from "react";
+import React from "react";
 import { Dropdown } from "semantic-ui-react";
 import PropTypes from "prop-types";
 
 const defaultPropsDropDown = [{ key: -1, text: "Все", value: "" }];
 
-class DropdownExampleSearchSelectionTwo extends Component {
-  render() {
-    const { itemsArea, value, onChange } = this.props;
-    let stateOptions = _.map(itemsArea, item => ({
-      key: item.id,
-      text: item.name,
-      value: item.id
-    }));
-    stateOptions = [...stateOptions, ...defaultPropsDropDown].sort(
-      item => item.key
-    );
-    return (
-      <Dropdown
-        value={value}
-        placeholder="Страна"
-        onChange={onChange}
-        search
-        selection
-        options={stateOptions}
-      />
-    );
-  }
+function DropdownExampleSearchSelectionTwo({itemsArea, value, onChange}) {
+  let stateOptions = _.map(itemsArea, item => ({
+    key: item.id,
+    text: item.name,
+    value: item.id
+  }));
+  stateOptions = [...stateOptions, ...defaultPropsDropDown].sort(
+    item => item.key
+  );
+
+  return (
+    <Dropdown
+      value={value}
+      placeholder="Страна"
+      onChange={onChange}
+      search
+      selection
+      options={stateOptions}
+    />
+  );
 }
 
 DropdownExampleSearchSelectionTwo.propTypes = {
