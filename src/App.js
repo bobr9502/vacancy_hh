969d265fcd24a996d8ed9da04039a269d2e9fc57
Andrey Vinokurov <vinokurov_and@mail.ru>
2019-04-08 22:46:18 +0300

    Refactoring. area => areas

--- src/App.js
+++ src/App.js
@@ -1,60 +1,61 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import ListVacancy from './components/ListVacancy';
-import { Container} from 'react-grid-system';
-import Menu from './components/Menu';
-import MenuSort from './components/MenuSort';
-import Filter from './containers/Filter';
-import Loader from './components/Loader';
-import * as VacancySelectors from './reducers/Vacancy';
-import * as AreaSelectors from './reducers/Area';
-import * as CurrencySelectors from './reducers/Currency';
-import * as DataActions from './actions/DataAction';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import ListVacancy from "./components/ListVacancy";
+import { Container } from "react-grid-system";
+import Menu from "./components/Menu";
+import MenuSort from "./components/MenuSort";
+import Filter from "./containers/Filter";
+import Loader from "./components/Loader";
+import * as VacancySelectors from "./reducers/Vacancy";
+import * as AreaSelectors from "./reducers/Area";
+import * as CurrencySelectors from "./reducers/Currency";
+import * as DataActions from "./actions/DataAction";
 
 class App extends Component {
-  state = { loading: true }
+  state = { loading: true };
 
-  componentDidMount()
-  {
+  componentDidMount() {
     if (!this.props.vacancy || !this.props.vacancy.length)
       this.props.onGetData(0);
-    this.setState({loading: false})
+    this.setState({ loading: false });
   }
 
   render() {
-    const { loading } = this.state
-      const {area, vacancy, listCurrency} = this.props
-    if (loading)
-        return ( <Loader /> )
+    const { loading } = this.state;
+    const { areas, vacancy, listCurrency } = this.props;
+    if (loading) return <Loader />;
     return (
-      <Container fluid className="container"> 
+      <Container fluid className="container">
         <nav>
-          <Menu active="Главная"/>
+          <Menu active="Главная" />
           <MenuSort />
-            <Filter itemsArea={area}/>
+          <Filter itemsArea={areas} />
         </nav>
         <main>
-            <ListVacancy itemsVacancy={vacancy} itemsCurrency={listCurrency}/>
+          <ListVacancy itemsVacancy={vacancy} itemsCurrency={listCurrency} />
         </main>
       </Container>
-      )
+    );
   }
 }
 
-function mapStateToProps(state)
-{
+function mapStateToProps(state) {
   return {
     vacancy: VacancySelectors.getVacancy(state),
-    area: AreaSelectors.getArea(state),
+    areas: AreaSelectors.getArea(state),
     listCurrency: CurrencySelectors.getCurrency(state)
- }
+  };
 }
 
-function mapDispatchToProps(dispatch)
-{
+function mapDispatchToProps(dispatch) {
   return {
-    onGetData: (numberPage) => { dispatch(DataActions.fetch(0)) }
-  }
+    onGetData: numberPage => {
+      dispatch(DataActions.fetch(0));
+    }
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
