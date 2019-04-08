309e778890a842ee9a4e74deecc9c425db2f92ec
Andrey Vinokurov <vinokurov_and@mail.ru>
2019-04-08 23:33:06 +0300

    Add Proptypes and defaultPropTypes

--- src/containers/Filter.js
+++ src/containers/Filter.js
@@ -1,40 +1,49 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Dropdown from "../components/Dropdown";
 import "../css/filter.css";
 import Loader from "../components/Loader";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 class Filter extends Component {
-  state = {loading: true};
+  state = { loading: true };
 
   componentDidMount() {
-    this.setState({loading: false});
+    this.setState({ loading: false });
   }
   render() {
-    const {loading} = this.state;
-    const {currentItem, itemsArea, onFilterVacancyCountry} = this.props;
+    const { loading } = this.state;
+    const { currentItem, itemsArea, onFilterVacancyCountry } = this.props;
 
-    if (loading) return <Loader type="block"/>;
+    if (loading) return <Loader type="block" />;
     return (
       <div className="filter">
         <div className="header item">Фильтр</div>
         <Dropdown
-            value={currentItem}
-            itemsArea={itemsArea}
-            onChange={onFilterVacancyCountry}
+          value={currentItem}
+          itemsArea={itemsArea}
+          onChange={onFilterVacancyCountry}
         />
       </div>
     );
   }
 }
 
+Filter.propTypes = {
+  currentItem: PropTypes.string
+};
+
+Filter.defaultPropTypes = {
+  itemsArea: -1
+};
+
 export default connect(
-    state => ({
-      currentItem: state.filterVacancyCountry
+  state => ({
+    currentItem: state.filterVacancyCountry
   }),
-    dispatch => ({
-      onFilterVacancyCountry: (event, data) => {
-        dispatch({type: "VACANCY_FILTER_COUNTRY", payload: data.value});
-      }
+  dispatch => ({
+    onFilterVacancyCountry: (event, data) => {
+      dispatch({ type: "VACANCY_FILTER_COUNTRY", payload: data.value });
+    }
   })
 )(Filter);
